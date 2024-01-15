30465c7460ae2442bc221163dd22d85cfd2d9f95
Spencer Stewart <spencerjamesstewart@gmail.com>
2024-01-14 23:14:13 -0600

    Reorganize app.js and create index route.

--- src/server/app.js
+++ src/server/app.js
@@ -1,18 +1,26 @@
+// Requires
 const path = require("path");
+const express = require("express");
+
+// Initialize express app
+const app = express();
+
+// Environment variables
 const envPath =
   process.env.NODE_ENV === "production"
     ? ".env.production"
     : ".env.development";
 require("dotenv").config({ path: path.resolve(process.cwd(), envPath) });
+const { PORT } = process.env;
 
-const express = require("express");
-const app = express();
-const port = process.env.PORT;
+// Set view engine
+app.set("view engine", "ejs");
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// Use routes
+const indexRoutes = require("./routes/indexRoutes");
+app.use("/", indexRoutes);
 
-app.get("/test", (req, res) => {
-  res.json({ message: "Test endpoint reached" });
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
