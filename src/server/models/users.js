3b6bbf36a0f5748c487207ca49a24c2ff360ac20
Spencer Stewart <spencerjamesstewart@gmail.com>
2024-01-28 08:46:02 -0600

    fix: establish association between Users and Screenshots

--- src/server/models/users.js
+++ src/server/models/users.js
@@ -1,44 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    "User",
-    {
-      // User ID: Primary key, auto-incremented integer
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      // Name: String field, cannot be null
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      // Email: String field, unique and cannot be null
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      // createdAt: Timestamp, automatically set to current time on creation
-      createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-      },
-      // updatedAt: Timestamp, automatically updated to current time on record update
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-      },
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    }, name: {
+      type: DataTypes.STRING, allowNull: false,
+    }, email: {
+      type: DataTypes.STRING, allowNull: false, unique: true,
+    }, createdAt: {
+      type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW,
+    }, updatedAt: {
+      type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW,
     },
-    {
-      // Model options
-    },
-  );
+  }, {
+    // Model options
+  },);
 
-  // Define any model associations here (if necessary)
+  User.associate = (models) => {
+    User.hasMany(models.Screenshot, {
+      foreignKey: "userId", as: "screenshots", onDelete: "CASCADE",
+    });
+  };
 
   return User;
 };
