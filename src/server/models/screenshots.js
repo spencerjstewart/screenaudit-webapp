2545b285f3e25725810773e9a7bf6cb6d1f178f8
Spencer Stewart <37820157+spencerjstewart@users.noreply.github.com>
2024-01-28 09:36:06 -0600

    fix: establish association between Users and Screenshots (#1)

--- src/server/models/screenshots.js
+++ src/server/models/screenshots.js
@@ -1,41 +1,29 @@
 module.exports = (sequelize, DataTypes) => {
-  const Screenshot = sequelize.define(
-    "Screenshot",
-    {
-      // Screenshot ID: Primary key, auto-incremented integer
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
+  const Screenshot = sequelize.define("Screenshot", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    }, userId: {
+      type: DataTypes.INTEGER, allowNull: false, references: {
+        model: 'Users', key: 'id',
       },
-      // User ID: Foreign key linking to User model
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Users', // References the Users table
-          key: 'id',
-        },
-      },
-      // Image URL: String field to store the URL of the screenshot
-      imageUrl: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      // Timestamp: Timestamp of when the screenshot was taken
-      timestamp: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-      },
+    }, imageUrl: {
+      type: DataTypes.STRING, allowNull: false,
     },
-    {
-      // Model options
-    },
-  );
 
-  // Define any model associations here (if necessary)
+    timestamp: {
+      type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW,
+    },
+  }, {},);
+
+  Screenshot.associate = (models) => {
+    Screenshot.belongsTo(models.User, {
+      foreignKey: "userId",
+      as: "user",
+    });
+  }
 
   return Screenshot;
 };
