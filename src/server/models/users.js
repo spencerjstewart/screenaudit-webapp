fe105790b719df5cfc6e7d7b6db40b5d712abb62
Spencer Stewart <spencerjamesstewart@gmail.com>
2024-01-28 21:22:33 -0600

    fix: define associations correctly

--- src/server/models/users.js
+++ src/server/models/users.js
@@ -1,28 +1,43 @@
+/**
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ */
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-    }, name: {
-      type: DataTypes.STRING, allowNull: false,
-    }, email: {
-      type: DataTypes.STRING, allowNull: false, unique: true,
-    }, createdAt: {
-      type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW,
-    }, updatedAt: {
-      type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW,
+  return sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
     },
-  }, {
-    // Model options
-  },);
-
-  User.associate = (models) => {
-    User.hasMany(models.Screenshot, {
-      foreignKey: "userId", as: "screenshots", onDelete: "CASCADE",
-    });
-  };
-
-  return User;
+    {
+      // Model options
+    },
+  );
 };
