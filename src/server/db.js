8ad23511200a3778e8f400f6b5872c6859dac17e
Spencer Stewart <spencerjamesstewart@gmail.com>
2024-01-28 20:18:53 -0600

    chore: fix db credentials for development

--- src/server/db.js
+++ src/server/db.js
@@ -6,13 +6,14 @@ if (process.env.NODE_ENV === "production") {
   require("dotenv").config({ path: "../../.env.development" });
 }
 
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_DIALECT } = process.env;
 
 const connection = mysql.createConnection({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
+  dialect: DB_DIALECT,
 });
 
 connection.connect((error) => {
