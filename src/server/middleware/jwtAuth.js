c3c21708304df6577aee3d8bd50e4b12482f6b8d
Spencer Stewart <spencerjamesstewart@gmail.com>
2024-02-03 17:22:12 -0600

    refactor: set user details on user object on req instead of looking up user every time

--- src/server/middleware/jwtAuth.js
+++ src/server/middleware/jwtAuth.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-const { User } = require("../models");
 
 async function authenticateToken(req, res, next) {
   const token = req.cookies.token;
@@ -7,17 +6,12 @@ async function authenticateToken(req, res, next) {
   if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-    if (err) return res.sendStatus(403);
-
-    try {
-      const user = await User.findByPk(decoded.id);
-      if (!user) return res.sendStatus(404);
-
-      req.user = user;
-      next();
-    } catch (error) {
-      return res.sendStatus(500);
+    if (err || !decoded || !decoded.id) {
+      return res.sendStatus(403);
+    } else {
+      req.user = { id: decoded.id, ...decoded };
     }
+    next();
   });
 }
 
