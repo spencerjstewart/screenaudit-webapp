85292574f11e3433110a72f6a209a0371d0da3fe
Spencer Stewart <37820157+spencerjstewart@users.noreply.github.com>
2024-02-15 23:03:35 -0600

    feat: pass baseUrl to dashboard to create nav links (#18)

--- src/server/controllers/dashboardController.js
+++ src/server/controllers/dashboardController.js
@@ -1,5 +1,5 @@
 exports.dashboardPage = async (req, res) => {
   const user = req.user;
   const { name, email } = user;
-  res.render("dashboard", { name, email });
+  res.render("dashboard", { name, email, baseUrl: process.env.BASE_URL });
 };
